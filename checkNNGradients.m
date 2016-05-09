3da26d77a8d866f5257537d32bd1cc8b7f57da61
Charles Araya <charlesaraya@gmail.com>
2016-05-09 19:00:09 +0200

    Update: reformat code

--- checkNNGradients.m
+++ checkNNGradients.m
@@ -1,12 +1,10 @@
 function checkNNGradients(lambda)
-%CHECKNNGRADIENTS Creates a small neural network to check the
-%backpropagation gradients
+%   CHECKNNGRADIENTS Creates a small neural network to check the backpropagation gradients
 %   CHECKNNGRADIENTS(lambda) Creates a small neural network to check the
 %   backpropagation gradients, it will output the analytical gradients
 %   produced by your backprop code and the numerical gradients (computed
 %   using computeNumericalGradient). These two gradient computations should
 %   result in very similar values.
-%
 
 if ~exist('lambda', 'var') || isempty(lambda)
     lambda = 0;
@@ -28,14 +26,12 @@ y  = 1 + mod(1:m, num_labels)';
 nn_params = [Theta1(:) ; Theta2(:)];
 
 % Short hand for cost function
-costFunc = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, ...
-                               num_labels, X, y, lambda);
+costFunc = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
 
 [cost, grad] = costFunc(nn_params);
 numgrad = computeNumericalGradient(costFunc, nn_params);
 
-% Visually examine the two gradient computations.  The two columns
-% you get should be very similar. 
+% Visually examine the two gradient computations.  The two columns you get should be very similar. 
 disp([numgrad grad]);
 fprintf(['The above two columns you get should be very similar.\n' ...
          '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
--- predictOneVsAll.m
+++ predictOneVsAll.m
@@ -1,6 +1,6 @@
 function p = predictOneVsAll(all_theta, X)
-%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
-%are in the range 1..K, where K = size(all_theta, 1). 
+%  PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%  are in the range 1..K, where K = size(all_theta, 1). 
 %  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
 %  for each example in the matrix X. Note that X contains the examples in
 %  rows. all_theta is a matrix where the i-th row is a trained logistic
@@ -10,21 +10,13 @@ function p = predictOneVsAll(all_theta, X)
 
 m = size(X, 1);
 num_labels = size(all_theta, 1);
-
-% You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
-% Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-
 [probs p] =  max((X * all_theta')');
-
 p = p';
+
 % =========================================================================
+
 end
